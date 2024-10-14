7ce5769c01a72f607f48a157d95dc5c88b464afc
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-10-14 16:50:54 +0300

    chore: create `command line interface` for web scraping

---
+++ app/app.py
@@ -0,0 +1,15 @@
+class App:
+    def __init__(self) -> None:
+        pass
+
+    def universities(self):
+        print("debug: universities()")
+    
+    def degrees(self):
+        print("debug: degrees()")
+    
+    def reset(self):
+        print("debug: reset()")
+    
+    def clean(self):
+        print("debug: clean()")
